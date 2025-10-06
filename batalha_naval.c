4b8ae2f141664572b79ce404aca7665c55fdc41f
Daniel Jesus <140974618+danielj13sus@users.noreply.github.com>
2025-10-06 20:03:41 -0300

    Habilidades especiais e áreas de efeito

--- batalha_naval.c
+++ batalha_naval.c
@@ -1,49 +1,56 @@
 #include <stdio.h>
-#include <stdlib.h>
-
+#include <stdlib.h> 
+#include <string.h> 
 #define BOARD_SIZE 10
 #define SHIP_SIZE 3
+#define SKILL_MATRIX_SIZE 7 
 
 typedef enum {
     WATER = 0,
-    SHIP = 3
+    SHIP = 3,
+    SKILL_AREA = 5
 } BoardCell;
 
 void initialize_board(int board[BOARD_SIZE][BOARD_SIZE]);
-void display_board(const int board[BOARD_SIZE][BOARD_SIZE]);
 int place_ship(int board[BOARD_SIZE][BOARD_SIZE], int start_row, int start_col, int size, char orientation);
+
+void generate_cone_skill(int matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int size);
+void generate_cross_skill(int matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int size);
+void generate_octahedron_skill(int matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int size);
+void apply_skill_on_board(int board[BOARD_SIZE][BOARD_SIZE], const int skill_matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int skill_size, int origin_r, int origin_c);
+
+void display_board(const int board[BOARD_SIZE][BOARD_SIZE]);
+
 
 /**
  * @brief
  */
 int main(void) {
     int board[BOARD_SIZE][BOARD_SIZE];
+    int skill_matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE];
 
     initialize_board(board);
+    place_ship(board, 0, 0, SHIP_SIZE, 'H');
+    place_ship(board, 2, 9, SHIP_SIZE, 'V');
+    place_ship(board, 2, 1, SHIP_SIZE, 'D');
+    place_ship(board, 1, 6, SHIP_SIZE, 'A');
 
-    printf("Iniciando a preparação do tabuleiro de Batalha Naval (Nível Intermediário)...\n");
+    printf("Tabuleiro inicial com navios:\n");
+    display_board(board);
+    printf("\n");
 
-    if (!place_ship(board, 0, 0, SHIP_SIZE, 'H')) {
-        fprintf(stderr, "Erro: Falha ao posicionar o navio Horizontal!\n");
-        return EXIT_FAILURE;
-    }
+    printf("Aplicando áreas de efeito das habilidades...\n");
 
-    if (!place_ship(board, 2, 9, SHIP_SIZE, 'V')) {
-        fprintf(stderr, "Erro: Falha ao posicionar o navio Vertical!\n");
-        return EXIT_FAILURE;
-    }
+    generate_cone_skill(skill_matrix, SKILL_MATRIX_SIZE);
+    apply_skill_on_board(board, skill_matrix, SKILL_MATRIX_SIZE, 2, 5);
 
-    if (!place_ship(board, 2, 1, SHIP_SIZE, 'D')) {
-        fprintf(stderr, "Erro: Falha ao posicionar o navio Diagonal!\n");
-        return EXIT_FAILURE;
-    }
+    generate_cross_skill(skill_matrix, SKILL_MATRIX_SIZE);
+    apply_skill_on_board(board, skill_matrix, SKILL_MATRIX_SIZE, 7, 7);
 
-    if (!place_ship(board, 1, 6, SHIP_SIZE, 'A')) {
-        fprintf(stderr, "Erro: Falha ao posicionar o navio Anti-Diagonal!\n");
-        return EXIT_FAILURE;
-    }
+    generate_octahedron_skill(skill_matrix, SKILL_MATRIX_SIZE);
+    apply_skill_on_board(board, skill_matrix, SKILL_MATRIX_SIZE, 6, 1);
 
-    printf("\nTabuleiro com 4 navios posicionados:\n");
+    printf("\nTabuleiro final com navios e áreas de habilidades aplicadas:\n");
     display_board(board);
 
     return EXIT_SUCCESS;
@@ -52,10 +59,14 @@ int main(void) {
 /**
  * @brief 
  */
-void initialize_board(int board[BOARD_SIZE][BOARD_SIZE]) {
-    for (int i = 0; i < BOARD_SIZE; i++) {
-        for (int j = 0; j < BOARD_SIZE; j++) {
-            board[i][j] = WATER;
+void generate_cone_skill(int matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int size) {
+    memset(matrix, 0, size * size * sizeof(int));
+    int center_col = size / 2;
+    for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+            if (c >= center_col - r && c <= center_col + r) {
+                matrix[r][c] = 1;
+            }
         }
     }
 }
@@ -63,6 +74,60 @@ void initialize_board(int board[BOARD_SIZE][BOARD_SIZE]) {
 /**
  * @brief
  */
+void generate_cross_skill(int matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int size) {
+    memset(matrix, 0, size * size * sizeof(int));
+    int center = size / 2;
+    for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+            if (r == center || c == center) {
+                matrix[r][c] = 1;
+            }
+        }
+    }
+}
+
+/**
+ * @brief 
+ */
+void generate_octahedron_skill(int matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int size) {
+    memset(matrix, 0, size * size * sizeof(int));
+    int center = size / 2;
+    for (int r = 0; r < size; r++) {
+        for (int c = 0; c < size; c++) {
+            if (abs(r - center) + abs(c - center) <= center) {
+                matrix[r][c] = 1;
+            }
+        }
+    }
+}
+
+/**
+ * @brief 
+ * @param origin_r 
+ * @param origin_c 
+ */
+void apply_skill_on_board(int board[BOARD_SIZE][BOARD_SIZE], const int skill_matrix[SKILL_MATRIX_SIZE][SKILL_MATRIX_SIZE], int skill_size, int origin_r, int origin_c) {
+    int skill_center = skill_size / 2;
+
+    for (int sr = 0; sr < skill_size; sr++) {
+        for (int sc = 0; sc < skill_size; sc++) {
+            if (skill_matrix[sr][sc] == 1) {
+                int board_r = origin_r + (sr - skill_center);
+                int board_c = origin_c + (sc - skill_center);
+
+                if (board_r >= 0 && board_r < BOARD_SIZE &&
+                    board_c >= 0 && board_c < BOARD_SIZE) {
+                    board[board_r][board_c] = SKILL_AREA;
+                }
+            }
+        }
+    }
+}
+
+void initialize_board(int board[BOARD_SIZE][BOARD_SIZE]) {
+    memset(board, 0, BOARD_SIZE * BOARD_SIZE * sizeof(int));
+}
+
 void display_board(const int board[BOARD_SIZE][BOARD_SIZE]) {
     printf("   ");
     for(int i = 0; i < BOARD_SIZE; i++) {
@@ -73,53 +138,35 @@ void display_board(const int board[BOARD_SIZE][BOARD_SIZE]) {
     for (int i = 0; i < BOARD_SIZE; i++) {
         printf("%d | ", i);
         for (int j = 0; j < BOARD_SIZE; j++) {
-            printf("%d ", board[i][j]);
+            char symbol = '?';
+            switch(board[i][j]) {
+                case WATER:      symbol = '~'; break;
+                case SHIP:       symbol = 'N'; break;
+                case SKILL_AREA: symbol = '*'; break;
+            }
+            printf("%c ", symbol);
         }
         printf("\n");
     }
 }
 
-/**
- * @brief 
- * @param orientation
- * @return 
- */
 int place_ship(int board[BOARD_SIZE][BOARD_SIZE], int start_row, int start_col, int size, char orientation) {
-
-    if (start_row < 0 || start_col < 0) return 0; 
-
+     if (start_row < 0 || start_col < 0) return 0;
     switch (orientation) {
-        case 'H':
-            if (start_col + size > BOARD_SIZE || start_row >= BOARD_SIZE) return 0;
-            break;
-        case 'V':
-            if (start_row + size > BOARD_SIZE || start_col >= BOARD_SIZE) return 0;
-            break;
-        case 'D':
-            if (start_row + size > BOARD_SIZE || start_col + size > BOARD_SIZE) return 0;
-            break;
-        case 'A':
-            if (start_row + size > BOARD_SIZE || start_col - size < -1) return 0;
-            break;
-        default:
-            fprintf(stderr, "Orientação '%c' inválida.\n", orientation);
-            return 0;
+        case 'H': if (start_col + size > BOARD_SIZE || start_row >= BOARD_SIZE) return 0; break;
+        case 'V': if (start_row + size > BOARD_SIZE || start_col >= BOARD_SIZE) return 0; break;
+        case 'D': if (start_row + size > BOARD_SIZE || start_col + size > BOARD_SIZE) return 0; break;
+        case 'A': if (start_row + size > BOARD_SIZE || start_col - size < -1) return 0; break;
+        default: return 0;
     }
-
     for (int i = 0; i < size; i++) {
         int r = start_row, c = start_col;
         switch (orientation) {
-            case 'H': c += i; break;
-            case 'V': r += i; break;
-            case 'D': r += i; c += i; break;
-            case 'A': r += i; c -= i; break;
+            case 'H': c += i; break; case 'V': r += i; break;
+            case 'D': r += i; c += i; break; case 'A': r += i; c -= i; break;
         }
-        if (board[r][c] != WATER) {
-            fprintf(stderr, "Validação falhou: Sobreposição de navios detectada em (%d, %d).\n", r, c);
-            return 0;
-        }
+        if (board[r][c] != WATER) return 0;
     }
-
     for (int i = 0; i < size; i++) {
         switch (orientation) {
             case 'H': board[start_row][start_col + i] = SHIP; break;
@@ -128,6 +175,5 @@ int place_ship(int board[BOARD_SIZE][BOARD_SIZE], int start_row, int start_col, 
             case 'A': board[start_row + i][start_col - i] = SHIP; break;
         }
     }
-
     return 1;
 }
